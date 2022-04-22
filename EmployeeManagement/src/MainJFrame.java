ed93b9543dd564ab02d7cd52c6c79056e09e8aa5
Huy T <39242676+huyxd@users.noreply.github.com>
2022-04-22 10:49:08 -0400

    Test Commit
    Small test

--- EmployeeManagement/src/MainJFrame.java
+++ EmployeeManagement/src/MainJFrame.java
@@ -43,7 +43,7 @@ public class MainJFrame extends javax.swing.JFrame {
             }
         };
         addWindowListener(exitListener);
-        //
+        // End of Adapted Code
         
     }
     
